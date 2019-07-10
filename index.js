9eafd94f0a3367465d29dbbdfd913f7913447abf
gergokutu <gergo.kutu@gmail.com>
2019-07-10 11:14:44 +0200

    farm setup

---
+++ index.js
@@ -0,0 +1,8 @@
+class Farm {
+    constructor(name) {
+      this.name = name
+    }
+  }
+
+  // export
+  module.exports.Farm = Farm
---
+++ index.spec.js
@@ -0,0 +1,14 @@
+// Add the most basic test to your index.spec.js
+// import
+const {Farm} = require('./index')
+
+//test
+test('A new Farm can be created', () => {
+  const farm = new Farm()
+  expect(farm).toBeTruthy()
+})
+
+test('A Farm can have a name', () => {
+    const farm = new Farm('TEST_NAME')
+    expect(farm.name).toBe('TEST_NAME')
+  })
